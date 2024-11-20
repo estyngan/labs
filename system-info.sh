7579fd9fccd682df7194ecc695aaa33eebec3d56
estelleNgankam <e3344923@student.dcccd.edu>
2024-11-20 10:12:36 -0600

    Add system information script

---
+++ system-info.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "System Information:"
+echo "-------------------"
+echo "Hostname: $(hostname)"
+echo "Operating System: $(uname -o)"
+echo "Kernel Version: $(uname -r)"
+echo "Architecture: $(uname -m)"
+echo "CPU Info: $(lscpu | grep 'Model name' | awk -F: '{print $2}' | sed 's/^ //')"
+echo "Total Memory: $(free -h | grep 'Mem:' | awk '{print $2}')"
