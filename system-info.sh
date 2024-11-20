436a102a22c6f51b744ccbebe2d2ef6a9837ec55
estyngan <157168889+estyngan@users.noreply.github.com>
2024-11-20 10:20:29 -0600

    Update system-info.sh

--- system-info.sh
+++ system-info.sh
@@ -7,3 +7,4 @@ echo "Kernel Version: $(uname -r)"
 echo "Architecture: $(uname -m)"
 echo "CPU Info: $(lscpu | grep 'Model name' | awk -F: '{print $2}' | sed 's/^ //')"
 echo "Total Memory: $(free -h | grep 'Mem:' | awk '{print $2}')"
+# repository fine on https://github.com/estyngan/labs.git
